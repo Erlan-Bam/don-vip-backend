8b7f554bca5122833260be84e1a4e122b629926e
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-06 15:14:50 +0500

    fix: fix user dto

--- src/user/dto/update-profile.dto.ts
+++ src/user/dto/update-profile.dto.ts
@@ -1,27 +1,65 @@
-import { IsOptional, IsString } from 'class-validator';
+import {
+  IsOptional,
+  IsString,
+  IsDateString,
+  IsPhoneNumber,
+  IsEmail,
+} from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateProfileDto {
   @IsOptional()
   id: number;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: 'First name of the user',
-    example: 'John',
+    example: 'Винсент',
   })
   @IsOptional()
   @IsString()
   first_name: string;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: 'Last name of the user',
-    example: 'Doe',
+    example: 'Вега',
   })
   @IsOptional()
   @IsString()
   last_name: string;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
+    description: 'Gender of the user',
+    example: 'Мужской',
+  })
+  @IsOptional()
+  @IsString()
+  gender: string;
+
+  @ApiPropertyOptional({
+    description: 'Date of birth (ISO 8601 format)',
+    example: '1992-11-02',
+  })
+  @IsOptional()
+  @IsDateString()
+  birth_date: string;
+
+  @ApiPropertyOptional({
+    description: 'Phone number in international format',
+    example: '+7 903 000 00 00',
+  })
+  @IsOptional()
+  @IsPhoneNumber('RU') // Adjust for your region
+  phone: string;
+
+  @ApiPropertyOptional({
+    description: 'User email (used as identifier)',
+    example: 'mail@gmail.com',
+  })
+  @IsOptional()
+  @IsEmail()
+  identifier: string;
+
+  @ApiPropertyOptional({
     type: 'string',
     format: 'binary',
     description: 'Avatar image file (PNG, JPG, WEBP)',
