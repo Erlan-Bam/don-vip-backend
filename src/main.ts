992ef9e0c8499713ab21156f60d0bcd989ae690f
Erlan-Bam <erlanzh.gg@gmail.com>
2025-05-07 21:26:45 +0500

    feat: updated

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      // whitelist: true,
       // forbidNonWhitelisted: true,
       transform: true,
       exceptionFactory: (errors) => {
