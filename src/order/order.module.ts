a0b7d91ada2fa239291b4deb8363b29f3d273832
Erlan-Bam <erlanzh.gg@gmail.com>
2025-05-10 14:03:10 +0500

    fix: smile service in order

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { SharedModule } from 'src/shared/shared.module';
+import { SmileService } from 'src/shared/services/smile.service';
 
 @Module({
   imports: [SharedModule],
-  providers: [OrderService],
+  providers: [OrderService, SmileService],
   controllers: [OrderController],
   exports: [OrderService],
 })
