8866d9aded116411ef597b0ff6e046bc8ebb04db
Erlan-Bam <erlanzh.gg@gmail.com>
2025-05-10 14:33:05 +0500

    fix: deleted ip

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import axios from 'axios';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -35,12 +34,6 @@ async function bootstrap() {
     ],
     exposedHeaders: ['Authorization'],
   });
-
-  const getPublicIp = async () => {
-    const res = await axios.get('https://api.ipify.org?format=json');
-    console.log('Public IP:', res.data.ip);
-  };
-  await getPublicIp();
 
   const config = new DocumentBuilder()
     .setTitle('Auth API')
