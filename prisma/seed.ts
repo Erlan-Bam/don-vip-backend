beec4a753dc817e8b675060ae60d432072c43565
Erlan-Bam <erlanzh.gg@gmail.com>
2025-05-20 20:14:10 +0500

    feat: updated

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Reset auto-increment for User.id to start from 20000
+  await prisma.$executeRawUnsafe(`
+    ALTER SEQUENCE "Order_id_seq" RESTART WITH 20000;
+  `);
+
+  // Optional: create some seed users
+  await prisma.order.create({
+    data: {
+      user_id: 1, // замените на существующий user.id
+      product_id: 1, // замените на существующий product.id
+      item_id: 123,
+      payment: 'tbank',
+    },
+  });
+}
+
+main()
+  .then(() => {
+    console.log('Seeding completed.');
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- src/payment/dto/pagsmile-create-payin.dto.ts
+++ src/payment/dto/pagsmile-create-payin.dto.ts
@@ -8,7 +8,7 @@ export class PagsmileCreatePayinDto {
   amount: string;
 
   @ApiProperty({
-    example: '550e8400-e29b-41d4-a716-446655440000',
+    example: 20002,
     description: 'UUID of the order in the database',
   })
   @Type(() => Number)
