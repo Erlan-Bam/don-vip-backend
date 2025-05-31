c80a51cae2725172e59498e22f5fa8b6f339b9cb
Erlan-Bam <erlanzh.gg@gmail.com>
2025-06-01 00:33:40 +0500

    feat: mistake

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -22,7 +22,7 @@ export class PaymentService {
   ) {
     const backendURL =
       this.configService.get<string>('NODE_ENV') === 'production'
-        ? 'https://api.don-vip.online/api'
+        ? 'https://api.don-vip.online'
         : 'http://localhost:6001';
     this.backendURL = backendURL;
     const baseURL =
@@ -66,7 +66,7 @@ export class PaymentService {
         content: `Донат на сайте Don Vip на сумму: ${data.amount}`,
         buyer_id: this.merchantId,
         trade_type: 'WEB',
-        return_url: 'https://don-vip.online/ru/payment/success',
+        return_url: 'https://don-vip.online/payment/success',
         version: '2.0',
       },
       {
