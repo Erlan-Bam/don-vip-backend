53fd81251265539d183ae87dcf33a671ee625b29
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-06-09 14:05:55 +0500

    fix: fix create banner dto

--- src/banners/dto/create-banner.dto.ts
+++ src/banners/dto/create-banner.dto.ts
@@ -1,1 +1,15 @@
-export class CreateBannerDto {}
+import { IsString, IsUrl } from 'class-validator';
+
+export class CreateBannerDto {
+  @IsString()
+  @IsUrl({}, { message: 'Image must be a valid URL' })
+  image: string;
+
+  @IsString()
+  @IsUrl({}, { message: 'Mobile image must be a valid URL' })
+  mobileImage: string;
+
+  @IsString()
+  @IsUrl({}, { message: 'Button link must be a valid URL' })
+  buttonLink: string;
+}
