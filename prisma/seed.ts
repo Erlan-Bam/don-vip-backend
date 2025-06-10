863a6d6e26294a818b0d7fadef384ec6d91f3b7d
Erlan-Bam <erlanzh.gg@gmail.com>
2025-06-10 15:56:40 +0500

    fix: remove unused product and user update logic from seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,36 +3,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const products = await prisma.product.findMany();
-  const users = await prisma.user.findMany();
-  for (const p of products) {
-    const newImage = p.image.replace('don-vip.online', 'don-vip.com');
-    const newCurrencyImage = p.currency_image
-      ? p.currency_image.replace('don-vip.online', 'don-vip.com')
-      : null;
-
-    // 2) Обновляем каждую запись
-    await prisma.product.update({
-      where: { id: p.id },
-      data: {
-        image: newImage,
-        currency_image: newCurrencyImage,
-      },
-    });
-  }
-  for (const u of users) {
-    const newAvatar = u.avatar
-      ? u.avatar.replace('don-vip.online', 'don-vip.com')
-      : null;
-
-    // 2) Обновляем каждую запись
-    await prisma.user.update({
-      where: { id: u.id },
-      data: {
-        avatar: newAvatar,
-      },
-    });
-  }
+  await prisma.user.update({
+    where: { identifier: 'Hoyakap@gmail.com' },
+    data: {
+      role: 'Admin',
+    },
+  });
 }
 
 main()
