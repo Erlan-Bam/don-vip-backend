622c1b14917f43bf413513edb7d28e718ff5c007
Erlan-Bam <erlanzh.gg@gmail.com>
2025-06-10 12:09:26 +0500

    fix: add missing CORS origin for admin panel

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'https://don-vip.com',
       'https://admin-panel.don-vip.com',
+      'http://admin-panel.don-vip.com',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
